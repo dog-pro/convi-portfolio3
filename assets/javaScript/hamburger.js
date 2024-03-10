6f1ed2447a027be292df15d6e187ad8afcf7475c
msms <msr.warden@gmail.com>
2024-03-10 21:05:30 +0900

    new commit

--- assets/javaScript/hamburger.js
+++ assets/javaScript/hamburger.js
@@ -5,33 +5,6 @@ $(function(){
   });
 });
 
-// $(function(){
-//   $('.c-humburger__button').click(function(){
-//     $('.p-hamburgerPC').toggleClass('add');
-//     setTimeout(function() {
-//       $('.p-hamburgerPC').removeClass('add');
-//     }, 600); 
-//     // .add クラスのアニメーション時間に合わせて待機する
-//   });
-// });
-
-
-
-//　　　　　両方　　.c-humburger__button
-// 空室確認ボタン　.p-vacant
-// 　　　　　　　×　.p-hamburgerPC__close
-
-// ハンバーガーメニュー
-// $(".c-humburger__button, p-hamburgerPC__close").on('click',() => {
-//   let trg = $(".c-humburger__button");
-//   if(!$(".p-hamburgerPC").is(':animated')){
-//       trg.toggleClass('add');
-//       $('.l-sidemenu__back').toggleClass('open');
-//   }
-//   return false;
-// });
-
-
 // ハンバーガー内　kids
 $(function(){
   $('.slide').click(function(){
@@ -42,12 +15,9 @@ $(function(){
 // SP icon入れ替え
 $(function(){
   $('.p-humburgerSPbutton__img').click(function(){
-    // $(this).slideToggle(600);
     $('.p-humburgerSP__close').slideToggle(600);
-    // $('.p-humburgerSP').toggleClass(600);
   });
   $('.p-humburgerSP__close').click(function(){
-    // $(this).slideToggle(600);
     $('.p-humburgerSP__img').slideToggle(600);
   });
 });
